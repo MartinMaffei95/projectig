9daf42cf2a6a8fe87763239f5af1854d261375ca
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-02-02 14:59:36 -0300

    m

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   };
   const getUserData = (userID: string, token: string) => {
     fetch(
-      `https://graph.facebook.com/v15.0/${userID}?fields=id,first_name,last_name,attribution_read&access_token=${token}`
+      `https://graph.facebook.com/v15.0/${userID}?fields=id,first_name,last_name&access_token=${token}`
     )
       .then((res) => res.json())
       .then((data) => {
