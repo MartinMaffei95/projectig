e8a5619f6e37fde4720099c1f4e36f385f699cf8
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-02-02 13:28:07 -0300

    Added fb login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [pictures, setPictures] = useState<any>();
+  const token = import.meta.env.VITE_IG_TOKEN;
+
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  const onLoginClick = () => {
+    window.FB.login();
+  };
+
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: '422187410098220',
+        cookie: true,
+        xfbml: true,
+        version: 'v15.0',
+      });
+
+      FB.AppEvents.logPageView();
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }, []);
   return (
-    <div className="App">
+    <div className="App bg-slate-400">
       <header>Mi app de Instagram</header>
+      <div>
+        <button onClick={onLoginClick}>Login with Facebook</button>
+      </div>
+      <div className="flex flex-wrap w-screen h-screen gap-4 overflow-y-scroll">
+        {pictures && pictures.length > 0
+          ? pictures.map((p: any) => (
+              <div className="w-64 shadow-xl flex flex-col bg-slate-100 rounded overflow-hidden pb-4">
+                <div className="overflow-hidden w-full h-80 flex">
+                  <img
+                    src={p.media_url}
+                    alt={p.caption}
+                    className=" object-cover"
+                  />
+                </div>
+                <p>{p.caption}</p>
+              </div>
+            ))
+          : 'NO HAY DATOS PARA MOSTRAR'}
+      </div>
     </div>
   );
 }
